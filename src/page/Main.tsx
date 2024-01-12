34139e489d6e2adea1eefcb752d39e64aa19d2f9
Oh Ga Eun <dhrkdms4113@naver.com>
2024-01-12 23:01:29 +0900

    (#1) design: main 페이지 상품 리스트

---
+++ src/page/Main.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { ProductAllList_I } from "../interface/product_I";
+import { productAllGET } from "../api/product";
+import styled from "@emotion/styled";
+import Card from "../components/main/Card";
+import { Layout } from "../components/layout/Layout";
+import { Banner } from "../components/main/Banner";
+import Masonry from "react-masonry-css";
+
+const breakpointMasonryObj = {
+  default: 4,
+  720: 3,
+  450: 1,
+};
+
+export const Main = () => {
+  const [productList, setProductList] = useState<ProductAllList_I[] | null>(null);
+
+  useEffect(() => {
+    const productData = async () => {
+      try {
+        const res = await productAllGET();
+        setProductList(res.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    productData();
+  }, []);
+  return (
+    <Layout>
+      <Banner />
+      {productList === null || productList.length === 0 ? null : (
+        <ProductsListUl>
+          <Masonry
+            breakpointCols={breakpointMasonryObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {productList.map(product => {
+              return (
+                <Card
+                  key={product.product_id}
+                  image={product.image}
+                  store_name={product.store_name}
+                  product_name={product.product_name}
+                  price={product.price}
+                />
+              );
+            })}
+          </Masonry>
+        </ProductsListUl>
+      )}
+    </Layout>
+  );
+};
+
+const ProductsListUl = styled.ul`
+  width: 100%;
+  margin-top: 5rem;
+  .my-masonry-grid {
+    display: flex;
+    margin: 0 auto;
+    width: 80%;
+    gap: 30px;
+  }
+  .my-masonry-grid_column > li {
+    margin-bottom: 30px;
+  }
+`;
