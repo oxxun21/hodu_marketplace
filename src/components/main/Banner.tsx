d734ed7478a40cb20918c92a7ca7d77617b410d5
Oh Ga Eun <dhrkdms4113@naver.com>
2024-01-17 14:04:50 +0900

    (#1) design: 홈 배너 디자인 변경

--- src/components/main/Banner.tsx
+++ src/components/main/Banner.tsx
@@ -3,11 +3,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import banner_1 from "../../assets/home_banner_1.webp";
 import banner_2 from "../../assets/home_banner_2.webp";
-import banner_3 from "../../assets/home_banner_3.webp";
+import banner_4 from "../../assets/home_banner_4.webp";
 import { Autoplay } from "swiper/modules";
 import { css } from "@emotion/react";
 
-// 배너 디자인 변경
 export const Banner = () => {
   return (
     <Swiper modules={[Autoplay]} slidesPerView={1} loop={true} speed={500} autoplay={{ delay: 4000 }}>
@@ -18,7 +17,7 @@ export const Banner = () => {
         <img css={[Image]} src={banner_2} alt="자켓 컬렉션 진행 배너" />
       </SwiperSlide>
       <SwiperSlide>
-        <img css={[Image]} src={banner_3} alt="티셔츠 컬렉션 진행 배너" />
+        <img css={[Image]} src={banner_4} alt="티셔츠 컬렉션 진행 배너" />
       </SwiperSlide>
     </Swiper>
   );
