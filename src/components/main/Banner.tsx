a97f648cac4f8db456d955dc43073fb0030972d1
Oh Ga Eun <dhrkdms4113@naver.com>
2024-01-19 18:06:30 +0900

    (#1) docs: main 마크업 추가

--- src/components/main/Banner.tsx
+++ src/components/main/Banner.tsx
@@ -9,17 +9,19 @@ import { css } from "@emotion/react";
 
 export const Banner = () => {
   return (
-    <Swiper modules={[Autoplay]} slidesPerView={1} loop={true} speed={500} autoplay={{ delay: 4000 }}>
-      <SwiperSlide>
-        <img css={[Image]} src={banner_1} alt="호두마켓 이벤트 최대 30% 세일" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img css={[Image]} src={banner_2} alt="자켓 컬렉션 진행 배너" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img css={[Image]} src={banner_4} alt="티셔츠 컬렉션 진행 배너" />
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <Swiper modules={[Autoplay]} slidesPerView={1} loop={true} speed={500} autoplay={{ delay: 4000 }}>
+        <SwiperSlide>
+          <img css={[Image]} src={banner_1} alt="호두마켓 이벤트 최대 30% 세일" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img css={[Image]} src={banner_2} alt="자켓 컬렉션 진행 배너" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img css={[Image]} src={banner_4} alt="티셔츠 컬렉션 진행 배너" />
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
